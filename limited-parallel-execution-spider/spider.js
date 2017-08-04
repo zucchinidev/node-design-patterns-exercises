8d81164d8df474cd13bc8f57a3299e7097ecc3bb
Andrea Zucchini <azga84@gmail.com>
2017-08-04 18:52:09 +0200

    limited parallel execution with generic queue

---
+++ limited-parallel-execution-spider/spider.js
@@ -0,0 +1,99 @@
+'use strict'
+const request = require('request')
+const fs = require('fs')
+const mkdirp = require('mkdirp')
+const path = require('path')
+const utilities = require('../tools/url')
+const TaskQueue = require('./taskQueue')
+const DEFAULT_NESTING = 1
+const LIMIT_PARALLEL_TASK = 2
+const downloadQueue = new TaskQueue(LIMIT_PARALLEL_TASK)
+const spidering = new Map()
+
+function spiderLinks (currentUrl, body, nesting, callback) {
+  let hasErrors = false
+  let completed = 0
+  if (nesting === 0) {
+    return process.nextTick(callback)
+  }
+
+  const links = utilities.getPageLinks(currentUrl, body)
+
+  links.forEach(link => {
+    downloadQueue.pushTask(taskDone => {
+      spider(link, nesting - 1, (err) => {
+        if (err) {
+          hasErrors = true
+          return callback(err)
+        }
+
+        completed += 1
+        if (!hasErrors && completed === links.length) {
+          return callback()
+        }
+        taskDone()
+      })
+    })
+  })
+}
+
+function saveFile (fileName, contents, callback) {
+  mkdirp(path.dirname(fileName), err => {
+    if (err) {
+      return callback(err)
+    }
+    fs.writeFile(fileName, contents, callback)
+  })
+}
+
+function download (url, fileName, callback) {
+  console.log(`Downloading ${url}`)
+  request(url, (err, response, body) => {
+    if (err) {
+      return callback(err)
+    }
+    saveFile(fileName, body, err => {
+      if (err) {
+        return callback(err)
+      }
+      console.log(`Downloaded and saved: ${url}`)
+      callback(null, body)
+    })
+  })
+}
+
+function spider (url, nesting, callback) {
+  if (spidering.has(url)) {
+    return process.nextTick(callback)
+  }
+  spidering.set(url, true)
+
+  const fileName = utilities.urlToFilename(url)
+  fs.readFile(fileName, 'utf-8', (err, body) => {
+    if (err) {
+      const fileNotFound = err.code === 'ENOENT'
+      if (!fileNotFound) {
+        return callback(err)
+      }
+
+      return download(url, fileName, (err, body) => {
+        if (err) {
+          return callback(err)
+        }
+        spiderLinks(url, body, nesting, callback)
+      })
+    }
+
+    spiderLinks(url, body, nesting, callback)
+  })
+}
+
+spider(process.argv[2], DEFAULT_NESTING, (err, filename, downloaded) => {
+  if (err) {
+    console.log(err)
+  } else if (downloaded) {
+    console.log(`Completed the download of "${filename}"`)
+  } else {
+    console.log(`"${filename}" was already downloaded`)
+  }
+})
---
+++ limited-parallel-execution-spider/taskQueue.js
@@ -0,0 +1,23 @@
+module.exports = class TaskQueue {
+  constructor (concurrency) {
+    this.concurrency = concurrency
+    this.running = 0
+    this.queue = []
+  }
+
+  pushTask (task) {
+    this.queue.push(task)
+    this.next()
+  }
+
+  next () {
+    while (this.running < this.concurrency && this.queue.length) {
+      const task = this.queue.shift()
+      task(() => {
+        this.running -= 1
+        this.next()
+      })
+      this.running += 1
+    }
+  }
+}
