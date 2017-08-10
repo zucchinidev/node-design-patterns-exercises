b2d88bdfe4ea52d8111c35e5069f085763c4e1b9
Andrea Zucchini <azga84@gmail.com>
2017-08-11 00:56:41 +0200

    Spider, limited parallel execution with promises

---
+++ spider-limited-parallel-execution-with-promises/TaskQueue.js
@@ -0,0 +1,25 @@
+module.exports = class TaskQueue {
+  constructor (concurrency) {
+    this.concurrency = concurrency
+    this.running = 0
+    this.queue = []
+  }
+
+  pushTask (task) {
+    this.queue.push(task)
+    this.next()
+  }
+
+  next () {
+    while (this.running < this.concurrency && this.queue.length) {
+      const task = this.queue.shift()
+
+      task().then(() => {
+        this.running -= 1
+        this.next()
+      })
+
+      this.running += 1
+    }
+  }
+}
---
+++ spider-limited-parallel-execution-with-promises/spider.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const promisify = require('../tools/promises').promisify
+const request = promisify(require('request'))
+const mkdirp = promisify(require('mkdirp'))
+const utilities = require('../tools/url')
+const TaskQueue = require('./TaskQueue')
+const writeFile = promisify(fs.writeFile)
+const readFile = promisify(fs.readFile)
+
+const DEFAULT_NESTING = 1
+const CONCURRENCY_TASK = 2
+const downloadQueue = new TaskQueue(CONCURRENCY_TASK)
+
+function spiderLinks (currentUrl, body, nesting) {
+  const links = utilities.getPageLinks(currentUrl, body)
+  let promise = Promise.resolve()
+  if (nesting === 0 || links.length === 0) {
+    return promise
+  }
+
+  return new Promise((resolve, reject) => {
+    let completed = 0
+    links.forEach(link => {
+      const task = () => {
+        return spider(link, nesting - 1)
+          .then(() => {
+            completed += 1
+            if (completed === links.length) {
+              resolve()
+            }
+          })
+          .catch(() => reject())
+      }
+      downloadQueue.pushTask(task)
+    })
+  })
+}
+
+function download (url, fileName) {
+  console.log(`Downloading ${url}`)
+  let body
+  return request(url)
+    .then(response => {
+      body = response.body
+      return mkdirp(path.dirname(fileName))
+    })
+    .then(() => writeFile(fileName, body))
+    .then(() => {
+      console.log(`Downloaded and saved: ${url}`)
+      return body
+    })
+}
+
+const spidering = new Map()
+
+function spider (url, nesting, callback) {
+  if (spidering.has(url)) {
+    return Promise.resolve()
+  }
+  spidering.set(url, true)
+
+  const fileName = utilities.urlToFilename(url)
+  return readFile(fileName, 'utf8')
+    .then(
+      body => (spiderLinks(url, body, nesting)),
+      err => {
+        if (err.code !== 'ENOENT') {
+          throw err
+        }
+        return download(url, fileName).then(body => spiderLinks(url, body, nesting))
+      })
+}
+
+spider(process.argv[2], DEFAULT_NESTING)
+  .then(() => {
+    console.log(`Download complete"`)
+  })
+  .catch(err => console.error(err))
